e52a1d76aff37f05bc11e716929d154de4fbf33d
yuntaeil-one <zergelling@naver.com>
2020-04-10 16:01:27 +0900

    board 객체 삭제

--- spring_board/src/main/java/com/web/board/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.web.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.web.board.vo.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Integer>{
-
-}
